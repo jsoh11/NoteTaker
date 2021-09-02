7a4b6d8b4b79c2248efea6d6ed692ce201b325f5
Jordan <jordansoh14@gmail.com>
2021-09-02 08:46:50 -0500

    added a delete funtion

--- server.js
+++ server.js
@@ -40,18 +40,18 @@ app.post('/api/notes', (req, res) => {
     }
 })
 app.delete('/api/notes', (req, res) => {
-    const tipId = req.params.tip_id;
-    readFromFile('./db/tips.json')
+    const dataId = req.params.id;
+    readFromFile('./db/db.json')
       .then((data) => JSON.parse(data))
       .then((json) => {
         // Make a new array of all tips except the one with the ID provided in the URL
-        const result = json.filter((tip) => tip.tip_id !== tipId);
+        const result = json.filter((dataDB) => id !== id);
   
         // Save that array to the filesystem
-        writeToFile('./db/tips.json', result);
+        writeToFile('./db/db.json', result);
   
         // Respond to the DELETE request
-        res.json(`Item ${tipId} has been deleted 🗑️`);
+        res.json(`Item ${dataId} has been deleted 🗑️`);
       });
   });
 
